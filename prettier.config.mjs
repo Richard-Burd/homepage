6b2fb4352272182f2d679400082ecc1d56ed4287
richardburd <rick.a.burd@gmail.com>
2026-07-24 15:49:19 -0700

    node v. engine pkg.json + prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+// https://prettier.io/docs/en/options.html
+/** @type {import('prettier').RequiredOptions} */
+const config = {
+  trailingComma: "es5",
+  semi: false,
+  singleQuote: true,
+  overrides: [
+    {
+      files: "Routes.*",
+      options: {
+        printWidth: 999,
+      },
+    },
+  ],
+  // tailwindConfig: './web/config/tailwind.config.js',
+  tailwindStylesheet: "./web/src/index.css",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
