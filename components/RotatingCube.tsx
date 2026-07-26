b540842ff94a6df24e921aa1e299445098e3eb1a
richardburd <rick.a.burd@gmail.com>
2026-07-26 16:53:29 -0700

    added 3D cube

---
+++ components/RotatingCube.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Edges } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import type { Mesh } from "three";
+
+function Cube() {
+  const meshRef = useRef<Mesh>(null);
+
+  useFrame((_, delta) => {
+    if (!meshRef.current) return;
+    meshRef.current.rotation.x += delta;
+    meshRef.current.rotation.y += delta * 0.6;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial color="#7f9bc7" />
+      <Edges color="#000000" threshold={15} />
+    </mesh>
+  );
+}
+
+export default function RotatingCube() {
+  return (
+    <div className="h-48 w-48">
+      <Canvas camera={{ position: [8, 7, 10], fov: 28 }}>
+        <ambientLight intensity={0.9} />
+        <directionalLight position={[4, 4, 4]} intensity={1} />
+        <Cube />
+      </Canvas>
+    </div>
+  );
+}
