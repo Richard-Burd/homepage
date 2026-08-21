1e34c370631418695d1f3782c9fb19131add4112
richardburd <rick.a.burd@gmail.com>
2026-08-21 11:34:29 -0700

    footer + pie chart fade

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default async function Footer() {
     <footer>
       <p
         dir="ltr"
-        className="mx-auto max-w-3xl px-4 py-8 text-center text-[1.4rem] text-zinc-700 dark:text-zinc-50"
+        className="mx-auto max-w-3xl px-4 py-8 text-center text-[0.98rem] text-zinc-700 min-[800px]:text-[1.4rem] dark:text-zinc-50"
       >
         {t.rich('copyright', {
           name: (chunks) => <span dir="ltr">{chunks}</span>,
